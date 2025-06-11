a286b6bb860b9e89881699067fbd0cf631710979
Renan Andrade de Almeida <renan@MacBook-Air-de-Renan.local>
2025-06-11 02:56:43 -0300

    fix: adiciona domínio pixabay.com na configuração do Next.js

---
+++ front/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["pixabay.com", "cdn.pixabay.com", "images.pexels.com"],
+  },
+};
+
+module.exports = nextConfig;
